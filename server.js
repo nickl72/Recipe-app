cf0bd8ce7169113ed6ae1cbe25acffb08082f668
David Williams <williamsdavida@johndeere.com>
2020-10-03 10:48:42 -0500

    Added routes, controllers, and updated server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const app = express();
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`${process.env.PORT}`)
+})
