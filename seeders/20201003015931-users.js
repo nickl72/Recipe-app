ca46c796f59ef207537c58079d21637dfce6d192
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-10-05 13:40:02 -0400

    Modified user seed file to contain hashed password

--- seeders/20201003015931-users.js
+++ seeders/20201003015931-users.js
@@ -4,13 +4,13 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [{
       name: 'Nick',
-      hashedpass: '123456',
+      hashedpass: '$2a$10$GbNktppXBRd5jUKjRZvs5eCMzGdUQGJ.yZyKurKLsT7nYnnayIgFe', // pass = 12345
       username: 'nlapointe',
       email: 'nick@email.com'
     },
   {
     name: 'David',
-    hashedpass: '654321',
+    hashedpass: '$2a$10$IJFjSvwtUadGb5xhe0eEcuddUd5WkqT7PbQ3YumMq.fTnmrTcZjfi', // pass=12345
     username: 'dwilliams',
     email: 'david@email.com'
   }],{})
