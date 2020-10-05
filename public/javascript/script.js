615291d9587e5ef1f7eb78aee500fc96722c7e68
David Williams <williamsdavida@johndeere.com>
2020-10-05 14:45:07 -0500

    Real basic multi line dom complete to add multiple ingredients and directions

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,5 +1,7 @@
 const addIngredient = document.querySelector('#addIngredient');
 const addStep = document.querySelector('#addStep');
+const ingredientDiv = document.querySelector('#ingredient');
+const stepDiv = document.querySelector('#step')
 
 
 addIngredient.addEventListener('click', addIngredientLine);
@@ -7,10 +9,32 @@ addStep.addEventListener('click', addStepLine);
 
 
 function addIngredientLine() {
-    console.log("clicked")
+    const name = document.createElement("input");
+    name.setAttribute("type", "text");
+    name.setAttribute("name", "name");
+    const amount = document.createElement("input");
+    amount.setAttribute("type", "number");
+    amount.setAttribute("name", "quantity");
+    const unit = document.createElement("input");
+    unit.setAttribute("type", "text");
+    unit.setAttribute("name", "units");
+    ingredientDiv.appendChild(name);
+    ingredientDiv.appendChild(amount);
+    ingredientDiv.appendChild(unit);
 };
 
 function addStepLine() {
-    console.log('clicked this one')
-
+    const stepNum = document.getElementsByClassName('stepNum');
+    console.log(stepNum);
+    number = parseInt(stepNum[stepNum.length-1].value);
+    const numberInc = number+1;
+    const stepNumber = document.createElement("input");
+    stepNumber.setAttribute("class", "stepNum")
+    stepNumber.setAttribute("type", "number");
+    stepNumber.setAttribute("value", `${numberInc}`);
+    const step = document.createElement('input');
+    step.setAttribute("type", "text");
+    step.setAttribute("name", "step");
+    stepDiv.appendChild(stepNumber);
+    stepDiv.appendChild(step);
 };
