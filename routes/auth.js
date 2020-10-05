3a65ea54a36f026aa1406dfe46c367e1e9a7a69d
David Williams <williamsdavida@johndeere.com>
2020-10-05 11:25:28 -0500

    Added ability to create recipe with multiple ingredients but not directions

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,10 @@ const ctrl = require('../controllers');
 router.get('/signup', ctrl.auth.renderSignUp);
 router.get('/login', ctrl.auth.renderLogin);
 router.get('/profile', ctrl.auth.renderProfile);
+router.get('/new', ctrl.auth.renderNewRecipe);
 
 
 router.post('/createUser', ctrl.auth.createUser);
+router.post('/newRecipe', ctrl.auth.createNewRecipe);
 
 module.exports = router;
